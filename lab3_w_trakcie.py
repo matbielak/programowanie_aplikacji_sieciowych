1dd43b6ca19205b5d04b83b9e6bc5708417eb2c4
matbielak <100089973+matbielak@users.noreply.github.com>
2025-06-13 11:44:20 +0200

    Update lab3_w_trakcie.py

--- lab3_w_trakcie.py
+++ lab3_w_trakcie.py
@@ -343,3 +343,66 @@ except OSError as e:
     print(f"An error occurred: {e}")
 finally:
     sock.close()
+
+
+### Zadanie 9
+# Klient: 
+import socket
+
+def main():
+    server_address = ('127.0.0.1', 2906)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_socket:
+        try:
+            ip_address = input("Podaj adres IP: ")
+
+            print(f"Wysyłanie adresu IP {ip_address} do serwera {server_address}")
+            client_socket.sendto(ip_address.encode(), server_address)
+
+
+            response, _ = client_socket.recvfrom(1024) 
+            print(f"Otrzymana nazwa hosta dla adresu {ip_address}: {response.decode()}")
+        
+        except Exception as e:
+            print(f"Wystąpił błąd: {e}")
+
+if __name__ == "__main__":
+    main()
+# Serwer:
+
+import socket, select, sys
+from time import gmtime, strftime
+
+HOST = '127.0.0.1'
+PORT = 2906
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+try:
+    sock.bind((HOST, PORT))
+except socket.error as msg:
+    print ('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+    sys.exit()
+
+print ("[%s] UDP ECHO Server is waiting for incoming connections on port %s ... " % (strftime("%Y-%m-%d %H:%M:%S", gmtime()), PORT))
+
+try:
+    while True:
+
+        data, address = sock.recvfrom(4096)
+        print('[%s] Received %s bytes from client %s. Data: %s' % (strftime("%Y-%m-%d %H:%M:%S", gmtime()), len(data), address, data))
+
+        if data:
+
+            try :
+
+                hostname = socket.gethostbyaddr(str(data.decode('utf-8')))
+                print(f"HOSTNAME: {hostname[0]}")
+                sent = sock.sendto(hostname[0].encode('utf-8'), address)
+                print ('[%s] Sent %s bytes bytes back to client %s.' % (strftime("%Y-%m-%d %H:%M:%S", gmtime()), sent, address))
+
+            except socket.herror:
+                sent = sock.sendto("Sorry, an error occurred in gethostbyaddr", address)
+                print ('[%s] Sent %s bytes bytes back to client %s.' % (strftime("%Y-%m-%d %H:%M:%S", gmtime()), sent, address))
+finally:
+    sock.close()
